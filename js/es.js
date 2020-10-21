9308a00cd6baaf414acd9ffd532b88357bfb035c
suganorm <73138626+suganorm@users.noreply.github.com>
2020-10-21 17:30:10 +0300

    1

---
+++ js/es.js
@@ -0,0 +1,19 @@
+(function() {
+    'use strict';
+
+    window.countryList = {
+        es: {
+            name: 'España',
+            code: 'es',
+            price: 39,
+            oldPrice: 78,
+            labelPrice: 'EUR',
+            phoneHelper: 'Por ejemplo, +34 123456789',
+            nameHelper: 'Alejandro Rodriguez',
+            nameError: 'Nombre (campo obligatorio)',
+            phoneError: 'Número incorrectamente especificado',
+            countryError: 'Debe introducir un país',
+            isDefault: true
+        }
+    };
+})();
---
+++ js/translater.js
@@ -0,0 +1,277 @@
+var new_lang = {
+"lt0": "Producto natural que normaliza<br>el nivel de glucosa en la sangre ",
+
+"lt1": "Llamada gtatuita",
+
+"lt2": "Normaliza",
+
+"lt3": "el nivel de glucosa en la sangre",
+
+"lt4": "Precio antiguo <span class=\"price_old\">45 euros</span>",
+
+"lt5": "Precio en oferta <span class=\"price_main\">20 euros</span>",
+
+"lt6": "Hacer el pedido",
+
+"lt7": "El resultado después de un ciclo de tratamiento ya",
+
+"lt8": "Absolutamente seguro, no tiene efectos secundarios",
+
+"lt9": "Completa considerablemente la alimentación",
+
+"lt10": "Ayuda y consultas de nuestros especialistas <br> durante todo el ciclo de tratamiento",
+
+"lt11": "Hacer el pedido con la entrega gratuita",
+
+"lt12": "Recibir una consulta gratuita",
+
+"lt13": "Precio antiguo:",
+
+"lt14": "Precio nuevo:",
+
+"lt15": "Introduzca sus datos en el formulario abajo",
+
+"lt16": "Hacer el pedido con el descuento",
+
+"lt17": "Consulta gratuita",
+
+"lt18": "Antes de la compra y sin obligaciones ningunas",
+
+"lt19": "Todos los días cada especialista ayuda a 20-30 consumidores de SugaNorm en las cuestiones de alimentación y toma del producto. Y para dedicar tiempo a cada uno, aumentamos la cantidad de los especialistas certificados para las consultas antes de la compra.",
+
+"lt20": "Hoy se han quedado",
+
+"lt21": "consultas",
+
+"lt23": "Qué resultados logramos",
+
+"lt24": "Estabilización del nivel de glucosa en la sangre durante 2 semanas del uso",
+
+"lt25": "Un 87% de las personas lograron resultados visibles después de 1 mes del uso",
+
+"lt26": "Más de un 90% de los clientes tienen una prolongación estable del efecto",
+
+"lt27": "La reducción hasta un 70% del presupuesto para la compra de los preparados ",
+
+"lt28": "Lograr los resultados con SugaNorm <br>es más fácil — ¡Empiezen ahora!",
+
+"lt29": "Camino hacia la salud con SugaNorm",
+
+"lt30": "Camino irresponsable",
+
+"lt31": "Hacer el pedido ahora en España",
+
+"lt33": "Qué síntomas de la diabetes existen ",
+
+"lt34": "Fátiga crónica",
+
+"lt35": "Disminución de la agudeza visual",
+
+"lt36": "Cambio del peso inexplicable",
+
+"lt37": "Las heridas que no se curan durante mucho tiempo",
+
+"lt38": "Problemas sexuales",
+
+"lt39": "Entumecimiento, hormigueo en las piernas o brazos",
+
+"lt40": "Sequedad de boca constante",
+
+"lt41": "Urgencia miccional frecuente",
+
+"lt42": "Hambre constante",
+
+"lt43": "Migrañas y dolor de cabeza",
+
+"lt44": "La causa es el alto nivel de glucosa en la sangre",
+
+"lt46": "Normaliza el nivel de glucosa",
+
+"lt47": "Libra de la fátiga crónica",
+
+"lt48": "Detiene la pérdida de la vista",
+
+"lt49": "Normaliza el peso",
+
+"lt50": "Recupera el metabolismo tisular",
+
+"lt51": "Recupera el libido",
+
+"lt52": "Normaliza el suministro de sangre en las extremidades",
+
+"lt53": "Elimina la sed inexplicable",
+
+"lt54": "Estabiliza el funcionamiento del sistena urinario",
+
+"lt55": "Elimina el hambre constante",
+
+"lt56": "Previene los dolores de cabeza",
+
+"lt57": "Cuanto antes normalizamos el nivel de glucosa tanto menos medicamentos tendremos que tomar – ¡Empezen ahora!",
+
+"lt58": "SugaNorm  ─ el remedio<br>para la lucha contra la diabetes",
+
+"lt59": "DESCUENTO",
+
+"lt63": "¡La cantidad de las mercancías <br>en el almacén más cercano a Usted <br>está limitado!",
+
+"lt66": "Hacer el pedido ahora en España",
+
+"lt68": "Qué será si descuidamos la diabetes",
+
+"lt69": "Nefropatía",
+
+"lt70": "Angiopatía",
+
+"lt71": "Polineuropatía",
+
+"lt72": "Retinopatía",
+
+"lt73": "Cetoacidosis",
+
+"lt74": "<strong>Nefropatía</strong>—las lesiones diabéticas de los elementos filtrantes de los riñones (glomérulos y túbulos), y también de los vasos sanguíneos que los alimentan. La nefropatía diabética es una de las causas frecuentes de la mortalidad prematura y discapcidad de los pacientes. La diabetes no es la única causa de los problemas con los riñones. Pero entre los que pasan por la diálisis y guardan cola para obtener un riñón donado para el transplante, la mayoría son diabéticos.",
+
+"lt75": "<strong>Angiopatía</strong> – es una complicación de la diabetes mellitus, que se manifesta en la lesión de todos los vasos sanguíneos en el organismo humano. Bajo la influencia de “los índices altos de glucosa”, que pasan por los vasos sanguíneos, las paredes de las arterias, venas y pequeños vasos capilares se destruyen. En relación con eso surge la hipoxia (la falta de oxígeno) del tejido cercundante y se afectan muchos órganos de los pacientes. La angiopatía del alto nivel lleva a la aparición de úlceras, necrosis de los tejidos (gangrena) y amputación de las extremidades. En caso del incumplimiento de las recomendaciones del médico la gangrena se desarrolla durante 5 años a partir del comienzo de angiopatía en un 90% de todos los pacientes con esta patología.",
+
+"lt76": "<strong>Polineuropatía</strong>— una lesión multiple de los nervios periféricos, se manifesta como las parálisis parciales, trastornos de sensibilidad y otros trastornos, que surgen principalmente en las extremidades inferiores del organismo de un enfermo. En las etapas más tardias de la enfermedad la polineuropatía de las extremidades inferiores puede llevar a la atrofia muscular, aparición de las úlceras tróficas y parálisis de las piernas, y después – la de los musculos respiratorios.",
+
+"lt77": "<strong>Retinopatía</strong>—una lesión de los vasos de la retina del globo ocular. Una complicación grave y muy frecuente de la diabetes, que puede llevar a la ceguera. Las complicaciones de la vista se observan en un 85% de los diabéticos. Cuando descubren la diabetes tipo 2 en las personas de la edad media y avanzada, en más de un 50% de los casos revelan las lesiones de los vasos que llevan la sangre a los ojos. Es la causa más frecuente de los nuevos casos de la ceguera entre las personas adultas de 20 a 74 años.",
+
+"lt78": "<strong>Cetoacidosis</strong>— una complicación aguda, que se desarrolla por la falta de insulina. Las células no pueden utilizar la glucosa como un fuente de energía, y el organismo pasa a la alimentación de las reservas de grasa. Durante la descomposición de las grasas se producen activamente los cuerpos cetónicos (cetonas). Cuando hay demasiadas cetonas que empiezan a circular en la sangre, los riñones no logran elimanarlos del organismo a tiempo, la acidez de la sangre sube. La persona empeza a sentirse debili y puede caer en coma. Tal situación requiere una intervención médica urgente, porque a menúdo lleva al fallecimiento.",
+
+"lt79": "No se descuiden — conserven su salud",
+
+"lt81": "Ayuda a eviar las consecuencias",
+
+"lt82": "<strong>Previene la nefropatía</strong>— gracias a la recuperación del metabolismo de los carbohidratos  en el organismo, los riñones no necesitan más llevar una sobrecarga para eliminar la glucosa superflua del organismo.",
+
+"lt83": "<strong>Previene la angiopatía </strong>— gracias a la reducción del nivel de la glucosa en la sangre permite hacer los vasos más elásticos, recupera su conducción natural.",
+
+"lt84": "<strong>Previene la polineuropatía </strong>— ayuda a prevenir las lesiones del sistema nervioso por medio de la reducción del efecto tóxico de la glucosa sobre todos los órganos y tejidos del organismo.",
+
+"lt85": "<strong>Previene la retinopatía </strong>— recupera la alimentación del nervio óptico gracias al funcionamiento correcto de los vasos del ojo.",
+
+"lt86": "<strong>Previene la cetoacidosis </strong>— asegura la alimentación completa de las células del organismo con la glucosa, que brinda una oportunidad de evitar la intoxicación del organismo.",
+
+"lt87": "Cuanto antes normalizamos el nivel de glucosa, tanto menos es el riesgo de tener las consecuencias peligrosas – ¡Empiezen ahora!",
+
+"lt88": "SugaNorm – el remedio<br>para la lucha contra la diabetes",
+
+"lt93": "¡La cantidad de mercancías en<br>el almacén más cercano a Usted<br>está limitado!",
+
+"lt96": "Hacer el pedido ahora en España",
+
+"lt98": "Cómo empezar a usar SugaNorm",
+
+"lt99": "Usted hace el pedido en el sistio web o llama",
+
+"lt100": "Nuestro consultor devuelve la llamada a Usted",
+
+"lt101": "Analizamos el estado de su salud",
+
+"lt102": "Calculamos el ciclo de tratamiento",
+
+"lt103": "Entrega al correo más cercano a Usted",
+
+"lt104": "Pago después de la entrega",
+
+"lt105": "Nuestro especialista asiste a Usted durante todo el ciclo de tratamiento",
+
+"lt106": "Su buena salud y longevidad",
+
+"lt107": "Con Usted trabajan solamente profesionales",
+
+"lt108": "Nicolás Pascual",
+
+"lt109": "Especialista principal de la empresa, diabético tipo 1, trabaja en la empresa más de 5 años, realizó más de 1500 consultas.",
+
+"lt110": "Recibir una consulta de Nicolás",
+
+"lt111": "Comentarios sobre Nicolás",
+
+"lt112": "Daniel Ortiz, 53 años, Madrid",
+
+"lt113": "Quiero transmitir al señor Pascual mi especial agradecimiento. Este hombre sabe no de oídas sobre los problemas relacionados con la diabetes mellitus. Estoy enfermo de la  diabetes desde hace mucho. Pero los problemas graves comenzaron en el último año. Los hombres me comprenderán: me volví impotente en lo que toca a la salud masculina. Comenzaron los problemas en la familia. Y todo esto en el fondo de los dolores de cabeza constantes, insomnia, irritabilidad, y como consecuencia — la apatía. Desde la primavera de este año empecé a tomar SugaNorm. Y si inicialmente estaba escéptico, ahora, después de varios meses, puedo recomendar tomar SugaNorm a todos los que antes sufrieron el mismo fracaso que yo en la lucha contra la diabetes. Mi vida familiar se arregló, por lo que quiero transmitir mi especial agradecimiento al señor Nicolás. En el trabajo fui ascendido hace poco.",
+
+"lt114": "Gladis Blanco, 41 año, Cadiz",
+
+"lt115": "Desde hace mucho buscaba un método de estabilización de glucosa en la sangre. Llevo más de 30 años con la diabetes. Tensión arterial, mala vista, el funcionamiento mediocre del hígado y riñones — todo esto está en el pasado. Llevo más de medio año tomando SugaNorm bajo observación de Nicolás Pascual. No solamente controla la toma semanal del fármaco, sino también compusó una dieta individual, gracias a la que hasta logré bajar los kilos superfluos. Otra vez gracias al señor Nicolás Pascual por una comprensión clara del problema, y apoyo moral en la lucha contra el morbo espantoso.",
+
+"lt116": "Eugenia Leon, 36 años, Marbella",
+
+"lt117": "El embarazo tardío difícil, la cesárea urgente, y el coma diabético sufrido, son antecedentes al diagnóstico “la diabétes mellitus”. Mi estado común en aquel tiempo se puede llamar así: la mamá enferma gravemente y un niño pequeño. Me alegro mucho de lo que todo esto se haya quedado en el pasado. El señor Nicolás Pascual comprendió de verdad mi problema, y juntos con SugaNorm logramos los buenos resultados durante 4 meses pasados. Pués y mi bebé está feliz de ver a su mamá jovial.",
+
+"lt118": "Inés Alonso Mora",
+
+"lt119": "Especialista de categoría superior, diabética tipo 2, trabaja en la empresa 4 años, ayudó a más de 900 clientes.",
+
+"lt120": "Recibir una consulta de Inés",
+
+"lt121": "Comentarios sobre Inés",
+
+"lt122": "Miguel Aguilar, 74 años, Huelva",
+
+"lt123": "Llevo más 20 años padeciendo de la diabetes. Probé todo lo que había recomendado el médico tratante, igual que las fórmulas de la medicina tradicional. El resultado es uno: el nvel de glucosa en la sangre no dejaba de subir. Mi estado se empeoraba. Dejé de salir a la calle, simplemente no tenía fuerzas. SugaNorm me ayudó a encontrar las fuerzas en mí mismo. ¡Así conocí a la señora Inés Alonso Mora. ¡Hasta la fecha, llevamos más de medio año conociendonos, y los resultados del uso de SugaNorm no dejan de alegrarme! El azúcar está dentro de los límites de la norma. Se recuperó el tonus vital, y no simplemente salgo a la calle, sino visito mi club de ajedrez favorito. ¡Estimada señora Inés, agradezco cordialmente a Usted por su delicadeza, bondad, comprensión y un trato humano caluroso!",
+
+"lt124": "Zoe Cruz, 48 años, Madrid",
+
+"lt125": "Después de un accidente de tráfico sufrí varias operaciones y como consecuencia fue un diagnóstico terrible — la diabetés mellitus. Desde el año 2003 estoy tomando la insulina. Tenía menos de 40 años todavía, y me sentía así como si hubiera vivido un siglo entero. A medida que el nivel de glucosa estaba subiendo, empezó a empeorarse la vista, al dignóstico la diabetes “se pegó” la hipertensión, como me parecía, para siempre. Pero como se dice: “cada hombre forja su felicidad”, y decidí salvarme yo misma. Ayudó el internet. En uno de los sitios wem leí una historia parecida a la mía, en la que un diabético usó SugaNorm y logró resultados significativos en la lucha cotra su enfermedad. Mi “guerra” con la diabetes no está acabada todavía, pero gracias a SugaNorm logré volver a empezar a trabajar, cuido a los nietos, y en el año proximo planeo un viaje a la Republica Checa. ¡Expreso mi profundo reconocimiento a señora Inés Alonso y SugaNorm por haberme devuelto “el gusto” por la vida!",
+
+"lt126": "Laura Herrera, 62 años, Barcelona",
+
+"lt127": "¡La señora Inés Alonso es una profesional con mayúsculas!Una especialista enterada, competente y comprensiva. Su trabajo se puede citarlo como ejemplo a cualquier especialista. Mi historia se parece a cientos de mil otras: disposición genética a la diabetes mellitus se agravó por las consecuencias del trabajo en la industria nociva. Cómo piedra caída del cielo en el año 2007 sonó el diagnóstico del médico tratante: la diabetes mellitus. Lo siguió una sucesión de los cambios de unos fármacos por otros. A decir francamente, no me ayudaba nada. El estado se empeoró: apareció la disnea, dolores de cabeza, vértigo y sequedad de boca. Es una parte pequeña de lo que viví. Sobre SugaNorm me enteré de casualidad haciendo cola a la consulta del endocrinólogo. Y la señora que contó de él, se destacaba visualmente de las demás diabéticas. Yo también decidí no darme por vencida y solicité apoyo en el sitio web de SugaNorm. Me recomendaron un ciclo de tratamiento de 6 meses. Hoy mi índice del nivel de glucosa es de 5.8, me siento excelente, todos los análisis están normales, y por todo eso vuelvo a dar muchas gracias a SugaNorm y a la señora Inés!",
+
+"lt128": "Victoria Torres",
+
+"lt129": "trabaja en la empresa desde el día de su fundación, lleva la cantidad más grande de los pacientes.",
+
+"lt130": "Norma Hidalgo",
+
+"lt131": "ayudará a Usted a componer una dieta diabética más correcta.",
+
+"lt132": "María de la Luz Pérez",
+
+"lt133": "especialista experimentada con vastos conocimientos.",
+
+"lt134": "Margarita Lopez",
+
+"lt135": "experta en las complicaciones de la diabetes mellitus.",
+
+"lt136": "Con SugaNorm es realmente más fácil",
+
+"lt137": "Seguro y no tiene efectos secundarios",
+
+"lt138": "No es aditivo",
+
+"lt139": "Universal para los diabéticos de ambos típos",
+
+"lt140": "Accesible y económico",
+
+"lt141": "SugaNorm – el remedio para<br> la lucha contra la diabetes",
+
+"lt146": "¡La cantidad de las mercancías en<br>el almacén más cercano Usted<br>está limitada!",
+
+"lt151": "Retrollamada",
+
+"lt152": "Haga su pedido y nuestro especialista se contactará con Usted para precisar los detalles",
+
+"lt154": "SugaNorm ─ el remedio para la lucha contra <br>la diabetes",
+
+"lt159": "¡La cantidad de las mercancías en <br> el almacén más cercano a Usted<br>está limitado!",
+
+"lt162": "SugaNorm ─ el remedio<br> para la lucha cotra la diabetes",
+
+"lt167": "¡La cantidad de las mercancías en<br>el almacén más cercano a Ustes <br>está limitada!",
+
+"lt170": "Gracias por su pedido"
+};
+function Translater () {
+               for (class_name in new_lang) {
+                var elements = document.getElementsByClassName(class_name);
+                if (elements.length) {
+                 for (key in elements) {
+                  elements[key].innerHTML = new_lang[class_name];
+                 }
+              }
+            }
+          };
